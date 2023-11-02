9448bb773a02f40f7dbe7c0a95391c5f9c52a885
abbouformations <135717843+abbouformations@users.noreply.github.com>
2023-11-02 09:32:01 +0100

    Update CustomerRepository.java

--- src/main/java/ma/formations/graphql/dao/CustomerRepository.java
+++ src/main/java/ma/formations/graphql/dao/CustomerRepository.java
@@ -7,4 +7,8 @@ import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByIdentityRef(String identityRef);
+
+    Optional<Customer> findByUsername(String username);
+
+
 }
