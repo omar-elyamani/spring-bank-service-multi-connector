5189067b7339d5e32300d547cd498051616e9646
abbouformations <135717843+abbouformations@users.noreply.github.com>
2023-11-02 12:28:38 +0100

    Update CustomerDto.java

--- src/main/java/ma/formations/graphql/dtos/customer/CustomerDto.java
+++ src/main/java/ma/formations/graphql/dtos/customer/CustomerDto.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ma.formations.graphql.dtos.bankaccount.BankAccountDto;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,4 +17,5 @@ public class CustomerDto {
     private String identityRef;
     private String firstname;
     private String lastname;
+    private List<BankAccountDto> bankAccounts;
 }
